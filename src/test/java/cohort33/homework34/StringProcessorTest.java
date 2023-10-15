c76169a707da7faeb5d4385e09d2a451978eeee5
Artur <artur040220@gmail.com>
2023-10-15 10:26:28 +0200

    34: Project was created, add pom.xml and Class for test

---
+++ src/test/java/cohort33/homework34/StringProcessorTest.java
@@ -0,0 +1,45 @@
+package cohort33.homework34;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StringProcessorTest {
+
+  private StringProcessor stringProcessor;
+
+  @BeforeEach
+  public void setUp() {
+    stringProcessor = new StringProcessor();
+  }
+
+  @Test
+  public void testTwoStringConcatination() {
+    assertEquals("ab", stringProcessor.concatenate("a", "b"));
+  }
+
+  @Test
+  public void testStringNull() {
+    assertEquals("anull", stringProcessor.concatenate("a", null));
+  }
+
+  @Test
+  public void testEmptyStrings() {
+    assertEquals("nullnull", stringProcessor.concatenate(null, null));
+  }
+
+  @Test
+  public void testStringLength() {
+    assertEquals(3, stringProcessor.getLength("abc"));
+  }
+
+  @Test
+  public void tsetReverseString() {
+    assertEquals("cba", stringProcessor.reverse("abc"));
+  }
+  @Test
+  public void testPolindrome(){
+    assertEquals(true,stringProcessor.isPalindrome("ABBA"));
+  }
+}
--- src/test/java/cohort33/lesson34_20231007/calculatorSimTest.java
+++ src/test/java/cohort33/lesson34_20231007/calculatorSimTest.java
@@ -15,8 +15,49 @@ public class calculatorSimTest {
   }
 
   @Test
-  public void testAdd() {
+  public void testAddTwoPositiveNumbers() {
     assertEquals(5, calculatorSim.add(2, 3));
   }
 
+  @Test
+  public void testAddTwoNegativeNumbers() {
+    assertEquals(-6, calculatorSim.add(-4, -2));
+  }
+
+  @Test
+  public void testAddTwoResultZero() {
+    assertEquals(0, calculatorSim.add(-4, 4));
+  }
+
+  @Test
+  public void testDivideByZero() {
+    assertEquals(-1, calculatorSim.divide(3,0));
+  }
+  @Test
+  public void testDivideTwoPositiveNumbers() {
+    assertEquals(3, calculatorSim.divide(30,10));
+  }
+  @Test
+  public void testMultiplyPositiveNumbers(){
+    assertEquals(24,calculatorSim.multyply(6,4));
+  }
+  @Test
+  public void testMultiplyNegativeNumbers(){
+    assertEquals(-24,calculatorSim.multyply(-6,4));
+  }
+  @Test
+  public void testMultiplyZero(){
+    assertEquals(0,calculatorSim.multyply(0,4));
+  }
+  @Test
+  public void testMultiplication(){
+    int a = 2;
+    int b = 3;
+    int result = a * b;
+    assertEquals(6, result);
+  }
+  @Test
+  public void testSubstuctPositive(){
+    assertEquals(0,calculatorSim.substract(3,3));
+  }
 }
