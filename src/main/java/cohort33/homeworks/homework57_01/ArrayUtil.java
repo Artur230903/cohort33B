e99d5dd9afdaa6e6f8422c9aea6aec915db0145a
Artur <artur040220@gmail.com>
2024-04-17 06:29:25 +0200

    Upload 2024-03-25

---
+++ src/main/java/cohort33/homeworks/homework57_01/ArrayUtil.java
@@ -0,0 +1,20 @@
+package cohort33.homeworks.homework57_01;
+
+import java.util.Random;
+
+public class ArrayUtil {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+
+    int[][] array = new int[5][5];
+
+    for (int i = 0; i < array.length; i++) {
+//      for (int j = 0; j < array[i].length; j++) {
+//        array[i][j] = new int[]{random.nextInt(1,100)}
+//      }
+
+    }
+
+  }
+}
